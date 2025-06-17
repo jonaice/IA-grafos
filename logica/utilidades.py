8f99a8de8b353c2a4b42ab42ccd33b2fb2c4f853
Jonatan Eduardo Ramirez Lopez <jonatanrammirez@gmail.com>
2025-06-17 00:19:07 -0600

    eliminadno archivos innecesarios

--- logica/utilidades.py
+++
@@ -1,9 +0,0 @@
-from shapely.geometry import LineString
-
-def es_visible(p1, p2, obstaculos_shapely):
-    linea = LineString([p1, p2])
-    for obstaculo in obstaculos_shapely:
-        if obstaculo.intersects(linea):
-            if not obstaculo.boundary.contains(obstaculo.intersection(linea)):
-                return False
-    return True
